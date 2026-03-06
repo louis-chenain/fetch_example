9af1032397dbceda0adbd7b902a7802ebf33a153
unknown <louis.chenain@ieseg.fr>
2026-03-06 14:44:00 +0100

    Add database connection using environment variables

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 from flask import Flask, jsonify
+import mysql.connector from dotenv 
+import load_dotenv 
+import os
 
 app = Flask(__name__)
+load_dotenv()
+
+def get_db_connection(): 
+    conn = mysql.connector.connect( 
+        host=os.getenv("DB_HOST"), 
+        user=os.getenv("DB_USER"), 
+        password=os.getenv("DB_PASS"), 
+        database=os.getenv("DB_NAME") 
+    ) 
+    return conn
 
 @app.route('/')
 def home():
